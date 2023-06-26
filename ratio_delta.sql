8b61523441afc10fef41d442241fcce8cad831e8
Laurent Lyaudet <Laurent.Lyaudet@gmail.com>
2023-06-26 15:37:19 +0200

    Update ratio_delta.sql with copyright notice

--- ratio_delta.sql
+++ ratio_delta.sql
@@ -1,3 +1,21 @@
+-- This file is part of RatioDelta library.
+
+-- RatioDelta is free software: you can redistribute it and/or modify
+-- it under the terms of the GNU Lesser General Public License as published by
+-- the Free Software Foundation, either version 3 of the License, or
+-- (at your option) any later version.
+
+-- RatioDelta is distributed in the hope that it will be useful,
+-- but WITHOUT ANY WARRANTY; without even the implied warranty of
+-- MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+-- GNU Lesser General Public License for more details.
+
+-- You should have received a copy of the GNU Lesser General Public License
+-- along with RatioDelta.  If not, see <http://www.gnu.org/licenses/>.
+
+-- ©Copyright 2023 Laurent Lyaudet
+
+
 -- It should be inlined.
 -- Maybe one day it will be a single assembly operation.
 CREATE FUNCTION fused_divide_add(
